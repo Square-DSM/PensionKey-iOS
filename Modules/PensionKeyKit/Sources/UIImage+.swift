b7198a053ec0760709572c9f0c5fe076dc17c4d5
juyeong525 <juyeongno1@naver.com>
2023-10-28 02:03:31 +0900

    ⚡️ :: 이미지 추가하며 생긴 버그 수정

---
+++ Modules/PensionKeyKit/Sources/UIImage+.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+public extension UIImage {
+    static let coin = PensionKeyKitAsset.Image.coin.image
+    static let gift = PensionKeyKitAsset.Image.gift.image
+    static let bulletinBoard = PensionKeyKitAsset.Image.bulletinBoard.image
+    static let account = PensionKeyKitAsset.Image.account.image
+}
