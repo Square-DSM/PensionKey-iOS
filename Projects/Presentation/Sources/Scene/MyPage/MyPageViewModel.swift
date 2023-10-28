d90987f7b5f1e6e92f4580dfd62a2ca92ba3964a
juyeong525 <juyeongno1@naver.com>
2023-10-28 17:24:16 +0900

    ⚡️ :: ViewModel 생성

---
+++ Projects/Presentation/Sources/Scene/MyPage/MyPageViewModel.swift
@@ -0,0 +1,21 @@
+import UIKit
+import Core
+import RxSwift
+import RxCocoa
+import RxFlow
+
+public class MyPageViewModel: BaseViewModel, Stepper {
+    public var steps = PublishRelay<Step>()
+    private let disposeBag = DisposeBag()
+
+    public init() {}
+
+    public struct Input {
+    }
+    public struct Output {
+    }
+
+    public func transform(_ input: Input) -> Output {
+        return Output()
+    }
+}
