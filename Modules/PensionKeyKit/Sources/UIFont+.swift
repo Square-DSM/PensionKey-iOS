b2a403c1766126f17066ced60934bd6fd00f9bfc
Byung jin <bjcho1503@naver.com>
2023-10-28 18:03:25 +0900

    ⚡️ :: font extension 추가

---
+++ Modules/PensionKeyKit/Sources/UIFont+.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+public extension UIFont {
+    
+    // MARK: Title
+    static let titleLarge = PensionKeyKitFontFamily.Pretendard.semiBold.font(size: 40)
+    static let titleMedium = PensionKeyKitFontFamily.Pretendard.semiBold.font(size: 24)
+    static let titleSmall = PensionKeyKitFontFamily.Pretendard.medium.font(size: 16)
+
+    //MARK: Body
+    static let bodyLarge = PensionKeyKitFontFamily.Pretendard.medium.font(size: 20)
+    static let bodyMedium = PensionKeyKitFontFamily.Pretendard.regular.font(size: 16)
+    static let bodySmall = PensionKeyKitFontFamily.Pretendard.regular.font(size: 14)
+}
