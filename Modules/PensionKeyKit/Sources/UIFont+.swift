69404d891aa6c896173365df9c8e0eaddcf27976
Byung jin <bjcho1503@naver.com>
2023-10-28 21:45:04 +0900

    ⚡️ :: 폰트 추가

--- Modules/PensionKeyKit/Sources/UIFont+.swift
+++ Modules/PensionKeyKit/Sources/UIFont+.swift
@@ -11,4 +11,7 @@ public extension UIFont {
     static let bodyLarge = PensionKeyKitFontFamily.Pretendard.medium.font(size: 20)
     static let bodyMedium = PensionKeyKitFontFamily.Pretendard.regular.font(size: 16)
     static let bodySmall = PensionKeyKitFontFamily.Pretendard.regular.font(size: 14)
+
+    // MARK: Label
+    static let labelSmall = PensionKeyKitFontFamily.Pretendard.medium.font(size: 14)
 }
