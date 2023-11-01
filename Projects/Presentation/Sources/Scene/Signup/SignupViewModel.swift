973a36afeb3f321dbd98fce1ba4b62de515753b1
juyeong525 <juyeongno1@naver.com>
2023-11-01 21:44:52 +0900

    ⚡️ :: Signup

--- Projects/Presentation/Sources/Scene/Signup/SignupViewModel.swift
+++ Projects/Presentation/Sources/Scene/Signup/SignupViewModel.swift
@@ -3,22 +3,41 @@ import Core
 import RxSwift
 import RxCocoa
 import RxFlow
+import Domain
 
 public class SignupViewModel: BaseViewModel, Stepper {
     public var steps = PublishRelay<Step>()
     private let disposeBag = DisposeBag()
 
-    public init() {}
+    private let signupUseCase: SignupUseCase
+
+    public init(signupUseCase: SignupUseCase) {
+        self.signupUseCase = signupUseCase
+    }
 
     public struct Input {
+        let nameText: Driver<String>
+        let idText: Driver<String>
+        let passwordText: Driver<String>
+        let signupButtonDidTap: Signal<Void>
         let loginButtonDidTap: Signal<Void>
     }
     public struct Output {
     }
 
     public func transform(_ input: Input) -> Output {
+        let info = Driver.combineLatest(input.nameText, input.idText, input.passwordText)
         input.loginButtonDidTap.asObservable()
             .map { _ in PensionStep.loginRequire }
+            .bind(to: steps)
+            .disposed(by: disposeBag)
+        input.signupButtonDidTap.asObservable()
+            .withLatestFrom(info)
+            .flatMap { name, id, password in
+                self.signupUseCase.execute(accountID: id, password: password, name: name)
+                    .andThen(Single.just(PensionStep.loginRequire))
+                    .catchAndReturn(PensionStep.errorRequire)
+            }
             .bind(to: steps)
             .disposed(by: disposeBag)
         return Output()
