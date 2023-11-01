ab09bd434a8175ceae9ba0801e8f950277ef9f33
juyeong525 <juyeongno1@naver.com>
2023-11-02 08:08:42 +0900

    ⚡️ :: Entity

--- Projects/Domain/Sources/Notice/Entity/NoticeDetailEntity.swift
+++ Projects/Domain/Sources/Notice/Entity/NoticeDetailEntity.swift
@@ -6,14 +6,12 @@ public struct NoticeDetailEntity: Equatable {
     public let content: String
     public let createdAt: String
     public let userAccountId: String
-    public let commentList: [CommentEntity]
 
-    public init(id: String, title: String, content: String, createdAt: String, userAccountId: String, commentList: [CommentEntity]) {
+    public init(id: String, title: String, content: String, createdAt: String, userAccountId: String) {
         self.id = id
         self.title = title
         self.content = content
         self.createdAt = createdAt
         self.userAccountId = userAccountId
-        self.commentList = commentList
     }
 }
