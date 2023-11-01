e4c3a76ce0ffdd5e6f11e02e5fdf61eed25d2d86
juyeong525 <juyeongno1@naver.com>
2023-11-01 16:50:04 +0900

    ⚡️ :: conflict 해결

--- Projects/Presentation/Sources/Scene/WriteNotice/WriteNoticeViewModel.swift
+++ Projects/Presentation/Sources/Scene/WriteNotice/WriteNoticeViewModel.swift
@@ -11,11 +11,31 @@ public class WriteNoticeViewModel: BaseViewModel, Stepper {
     public init() {}
 
     public struct Input {
+        let registerButtonSignal: Signal<Void>
+        let titleDriber: Driver<String>
+        let contentsDriber: Driver<String>
     }
     public struct Output {
+        let isRegisterAble: Signal<Bool>
     }
 
+    let isRegisterAble = PublishRelay<Bool>()
+
     public func transform(_ input: Input) -> Output {
-        return Output()
+        let info = Driver.combineLatest(input.titleDriber, input.contentsDriber)
+        info.asObservable()
+            .map { !$0.0.isEmpty && !$0.1.isEmpty }
+            .bind(to: isRegisterAble)
+            .disposed(by: disposeBag)
+        
+        input.registerButtonSignal.asObservable()
+            .subscribe(onNext: {
+                print("등록")
+            })
+            .disposed(by: disposeBag)
+
+        return Output(
+            isRegisterAble: isRegisterAble.asSignal()
+        )
     }
 }
